a8bf624aae5a6c893dc2a13358dd40182dccfdc2
Jagaa <jagaa1103@gmail.com>
2019-08-21 20:31:35 +0900

    try change header

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,18 @@
+import theme from "./../styles/theme";
 const Home = () => {
     return (
-        <div>Welcome</div>
+        <div className="container">
+            <h1>Welcome, I'm Enkhjargal</h1>
+            <style jsx>{`
+                .container {
+                margin: 50px;
+                }
+                .container :global(.btn) {
+                background: #000;
+                color: #fff;
+                }
+            `}</style>
+        </div>
     )
 }
 
